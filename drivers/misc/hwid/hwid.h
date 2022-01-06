e43a5ec05a9c8c50ba15ce5ed05da27c52911cd6
Arian <arian.kulmer@web.de>
2022-01-06 23:24:52 +0000

    drivers: misc: Introduce simplified xiaomi hwid driver
    Change-Id: I6ae7a76dcc687fe8fa9edb5c119182d2c32462be

---
+++ drivers/misc/hwid/hwid.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2021 XiaoMi, Inc.
+ *               2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef __HWID_H__
+#define __HWID_H__
+
+#include <linux/types.h>
+
+#define HARDWARE_PROJECT_UNKNOWN    0
+
+typedef enum {
+	CountryCN = 0x00,
+	CountryGlobal = 0x01,
+	CountryIndia = 0x02,
+	CountryJapan = 0x03,
+	INVALID = 0x04,
+	CountryIDMax = 0x7FFFFFFF
+} CountryType;
+
+uint32_t get_hw_version_platform(void);
+uint32_t get_hw_id_value(void);
+uint32_t get_hw_country_version(void);
+uint32_t get_hw_version_major(void);
+uint32_t get_hw_version_minor(void);
+uint32_t get_hw_version_build(void);
+
+#endif /* __HWID_H__ */
