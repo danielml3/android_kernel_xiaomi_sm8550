42e966b26c6c5632aa678e9dce5beee7be5b0410
danielml <daniel@danielml.dev>
2024-01-30 20:23:11 +0100

    drivers: misc: hwid: Implement product_name_get
    Change-Id: I17cf337fdc4ae81809234f1475d38cc3acd68b7b
    Signed-off-by: danielml <daniel@danielml.dev>

--- drivers/misc/hwid/hwid.h
+++ drivers/misc/hwid/hwid.h
@@ -27,5 +27,6 @@ uint32_t get_hw_country_version(void);
 uint32_t get_hw_version_major(void);
 uint32_t get_hw_version_minor(void);
 uint32_t get_hw_version_build(void);
+char* product_name_get(void);
 
 #endif /* __HWID_H__ */
